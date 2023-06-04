7ad58fcd7ebaf1ff4e63dbc41dcf7452324c81e6
KEITA <ksekimotonz@gmail.com>
2023-06-05 00:17:24 +0900

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,17 @@ function App() {
   // 追加：：useFormからformState:{errors}をimportする
   const { register,handleSubmit,formState:{errors}} = useForm();
 
-  const onSubmit = (data) => console.log("onSubmit:",data);
+  const onSubmit = (data) => console.log("onSubmit:", data);
+
+  const firstNameRules = {
+    required: "入力してください",
+    minLength:{value:6,message:"6文字以上,15文字以内で入力してください"},
+    maxLength: { value: 15, message:"6文字以上,15文字以内で入力してください"}
+  }
+
+  const emailRules = {
+    pattern: { value: /^[a-zA-Z0-9]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9]+)/, message:"@を含んだメールアドレスの形式で入力してください。"}
+  }
 
   return (
     <div className="App">
@@ -16,16 +26,16 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
 
         <label> First Name</label>
-        <input {...register("firstName", { maxLength: 15 })} />
-        {errors.firstName && <p>15文字以内で入力してください。</p>}
+        <input {...register("firstName", firstNameRules)} />
+        {errors.firstName && errors.firstName.message}
 
         <label> Last Name</label>
-        <input {...register("lastName", { maxLength: 15 })} />
-        {errors.lastName && <p>10文字以内で返してください</p>}
+        <input {...register("lastName", { required:true })} />
+        {errors.lastName && <p>名は入力必須です。</p>}
 
         <label> email</label>
-        <input {...register("email",{required:true})} />
-        {errors.email && <p>メールアドレスは必須です。</p>}
+        <input {...register("email", emailRules)} />
+        {errors.email && errors.email.message}
 
         <label> 年齢</label>
         <input {...register("age")} />
